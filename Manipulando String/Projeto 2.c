9b28cda0def93e03a7482a627c054bd1f89ea26c
marcosgs <mvinicius17112002@gmail.com>
2023-09-15 20:37:25 -0300

    Projeto 1 e 2

---
+++ Manipulando String/Projeto 2.c
@@ -0,0 +1,79 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+// Declarando um array de strings chamado 'nomes' com 20 strings de até 20 caracteres cada.
+char nomes[20][20] = {"macaa", "banana", "pera", "uva", "laranja", "abacaxi", "limao", "manga", "abacate", "kiwi", "cereja", "morango", "pessego", "goiaba", "melancia", "framboesa", "amora", "caqui", "figo", "papaya"};
+
+// Função 'ordenar' que recebe dois ponteiros de inteiros como parâmetros.
+void ordenar(int *trocas, int *comparacao)
+{
+    int x, y, r;
+    char aux[80];
+
+    *trocas = 0;
+    *comparacao = 0;
+
+    // Algoritmo de ordenação de bolha.
+    for (x = 0; x <= 19; x++)
+    {
+        for (y = x + 1; y <= 19; y++)
+        {
+            (*comparacao)++;
+            r = strcmp(nomes[x], nomes[y]);
+            if (r > 0)
+            {
+                strcpy(aux, nomes[x]);
+                strcpy(nomes[x], nomes[y]);
+                strcpy(nomes[y], aux);
+                (*trocas)++; // Incrementa o contador de trocas.
+            }
+        }
+    }
+}
+
+int main()
+{
+    // Abre um arquivo chamado "arquivoSaida.txt" para escrita.
+    FILE *arquivo = fopen("arquivoSaida.txt","w");
+
+    int x, trocas, comparacao;
+
+    // Imprime os nomes não ordenados na tela e no arquivo.
+    printf(" Vetor nao ordenado!\n\n");
+    fprintf(arquivo," Vetor nao ordenado!\n\n");
+    
+    for (x = 0; x <= 19; x++)
+    {
+        printf(" %s, ", nomes[x]);
+        fprintf(arquivo,"%s ,", nomes[x]);
+    }
+
+    ordenar(&trocas, &comparacao);
+
+    // Imprime os nomes ordenados na tela e no arquivo.
+    printf("\n\n Vetor ordenado!\n\n");
+    fprintf(arquivo,"\n\n Vetor ordenado!\n\n");
+    
+    for (x = 0; x <= 19; x++)
+    {
+        printf(" %s, ", nomes[x]);
+        fprintf(arquivo," %s, ", nomes[x]);
+    }
+     
+    // Imprime o número de trocas e comparações na tela e no arquivo.
+    printf("\n\nNumero de trocas: %d\n", trocas);
+    fprintf(arquivo,"\n\nNumero de trocas: %d\n", trocas);
+    printf("Numero de comparacoes realizadas foram : %d\n",comparacao);
+    fprintf(arquivo,"Numero de comparacoes realizadas foram : %d\n",comparacao);
+
+    // Imprime a string mediana e escreve no arquivo.
+    printf("A String mediana e: %s\n", nomes[9]);
+    fprintf(arquivo,"A String mediana e: %s\n", nomes[9]);
+
+    // Fecha o arquivo.
+    fclose(arquivo);
+
+    return 0;
+}
---
+++ Manipulando arquivos/Projeto 1.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+int main()
+{
+    // Abre o arquivo de entrada para leitura e o arquivo de saída para escrita.
+    FILE *arquivoEntrada = fopen("DadosEntrada.csv", "r");
+    FILE *arquivoSaida = fopen("SituacaoFinal.csv", "w");
+
+    float nota1, nota2, media;
+    char nome[50], telefone[50], curso[50], nota1_str[50], nota2_str[50], situacao[50];
+    char linha[256];
+    bool primeiraLinha = true;
+
+    if (arquivoEntrada == NULL || arquivoSaida == NULL)
+    {
+        printf("Erro ao abrir os arquivos!\n");
+        exit(1);
+    }
+
+    while (fgets(linha, sizeof(linha), arquivoEntrada) != NULL)
+    {
+        if (primeiraLinha)
+        {
+            primeiraLinha = false;
+            continue; // Ignora a primeira linha do arquivo.
+        }
+
+        // Lê os dados da linha do arquivo de entrada.
+        if (sscanf(linha, "%[^,],%[^,],%[^,],%f,%f", nome, telefone, curso, &nota1, &nota2) == 5)
+        {
+            // Calcula a média das notas.
+            media = (nota1 + nota2) / 2;
+        }
+
+        // Determina a situação do aluno com base na média.
+        if (media >= 7.0)
+        {
+            sprintf(situacao, "Aprovado!");
+        }
+        else
+        {
+            sprintf(situacao, "Reprovado!");
+        }
+
+        // Escreve os dados formatados no arquivo de saída.
+        fprintf(arquivoSaida, "%s,%0.2f,%s\n", nome, media, situacao);
+    }
+
+    // Fecha os arquivos.
+    fclose(arquivoEntrada);
+    fclose(arquivoSaida);
+
+    // Mensagem de conclusão.
+    printf("Compilacao realizada com sucesso!\n");
+
+    return 0;
+}
